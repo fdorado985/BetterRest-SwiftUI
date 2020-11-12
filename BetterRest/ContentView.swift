2c2909f564a55ca42d2a5126ca0ce52c27a52488
Juan Dorado <fdorado985@gmail.com>
2020-11-11 23:55:54 -0600

    Show the date picker on a Form

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
   @State private var wakeUp = Date()
 
   var body: some View {
-    DatePicker("Please enter a date", selection: $wakeUp)
+    Form {
+      DatePicker("Please enter a date", selection: $wakeUp)
+    }
   }
 }
 
