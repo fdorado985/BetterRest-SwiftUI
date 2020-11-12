ef3da11cf31c5b74904febdeae30ace0e3ad9721
Juan Dorado <fdorado985@gmail.com>
2020-11-12 01:21:58 -0600

    Change the VStack with a Form instead

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
 
   var body: some View {
     NavigationView {
-      VStack {
+      Form {
         Text("When do you want to wake up?")
           .font(.headline)
 
