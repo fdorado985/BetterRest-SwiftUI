059039bb583d9a7cefa64f901278b3d636b5ae4c
Juan Dorado <fdorado985@gmail.com>
2020-11-12 01:20:42 -0600

    Change the wake time to our default new computed property

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var wakeUp = Date()
+  @State private var wakeUp = defaultWakeTime
   @State private var sleepAmount = 8.0
   @State private var coffeeAmount = 1
   @State private var alertTitle = ""
   @State private var alertMessage = ""
   @State private var showingAlert = false
 
-  var defaultWakeTime: Date {
+  static var defaultWakeTime: Date {
     var components = DateComponents()
     components.hour = 7
     components.minute = 0
